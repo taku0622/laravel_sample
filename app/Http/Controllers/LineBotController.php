fe008e14f5c0b155b02b63112eeb0a92402afeaa
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-04 17:17:56 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -13,6 +13,6 @@ class LineBotController extends Controller
 
     public function parrot(Request $request)
     {
-        error_log(json_encode($request, JSON_UNESCAPED_UNICODE);
+        error_log("hello");
     }
 }
