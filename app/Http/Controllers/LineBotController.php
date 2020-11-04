f5cc59a89043b44ca488e69982fac8fea9c3938a
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-04 17:51:23 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 // ==========ここから追加==========
-use Illuminate\Support\Facades\Log;
+// use Illuminate\Support\Facades\Log;
 // ==========ここまで追加==========
 
 class LineBotController extends Controller
@@ -17,8 +17,8 @@ class LineBotController extends Controller
 
     public function parrot(Request $request)
     {
-        // error_log("hello");
-        Log::debug($request->header());
-        Log::debug($request->input());
+        error_log("hello");
+        // Log::debug($request->header());
+        // Log::debug($request->input());
     }
 }
