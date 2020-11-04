cdef9694bfde0aec130efc763ac1e658085dc0fd
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-04 17:02:04 +0900

    doen

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// ==========ここから追加==========
+use Illuminate\Support\Facades\Log;
+// ==========ここまで追加==========
+
 class LineBotController extends Controller
 {
     public function index()
     {
         return view('linebot.index');
+    }
+
+    public function parrot(Request $request)
+    {
+        // ==========ここから追加==========
+        Log::debug($request->header());
+        Log::debug($request->input());
+        // ==========ここまで追加==========
     }
 }
