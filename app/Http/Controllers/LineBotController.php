972636d07e7983a89cd1fc4ab708510fc32ae035
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-04 18:01:36 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Log;
 // ==========ここまで追加==========
 
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 class LineBotController extends Controller
 {
     public function index()
@@ -20,5 +23,13 @@ class LineBotController extends Controller
         error_log("hello");
         // Log::debug($request->header());
         // Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
     }
 }
