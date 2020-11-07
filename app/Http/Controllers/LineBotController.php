9311881dc7f8f7dba83fd804a88df2174d097331
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-07 22:51:22 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -84,7 +84,12 @@ class LineBotController extends Controller
             $replyText = $event->getText();
             $replyText = $name;
             $lineBot->replyText($replyToken, $replyText);
-            $lineBot->replyText($replyToken, $replyText);
+            $userId = $event->getUserId();
+            $pochi = Pet::find(1);
+            $pochi = json_decode(Pet::find(1), true);
+            $name = $pochi["name"];
+            $message = "名前を変更しました\n" . $name;
+            $lineBot->pushMessage($userId, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
             #############################################################
 
             #############################################################
