79bd35f70833bb0101c46019ba5b89983f3c9b78
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-07 22:37:43 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -8,7 +8,7 @@ use App\Services\dbConnection;
 
 use Illuminate\Http\Request;
 
-// use App\Models\Pet;
+use App\Models\Pet;
 // use App\Models\Conversation;
 
 use LINE\LINEBot;
@@ -72,6 +72,7 @@ class LineBotController extends Controller
             // error_log("text: " . $text);
             // $this->postToApp($userId, $replyToken, $text);
             $lineBot->replyText($replyToken, $replyText);
+            $lineBot->replyText($replyToken, $replyText);
             #############################################################
             // $replyToken = $event->getReplyToken();
             // $pochi = Pet::find(1);
@@ -89,114 +90,114 @@ class LineBotController extends Controller
         }
     }
 
-    public function watson($userId, $text)
-    {
-        define('TABLE_NAME_CONVERSATIONS', 'conversations');
-        ###############################
-        $data = array('input' => array("text" => $text));
+    // public function watson($userId, $text)
+    // {
+    //     define('TABLE_NAME_CONVERSATIONS', 'conversations');
+    //     ###############################
+    //     $data = array('input' => array("text" => $text));
 
-        // 前回までの会話のデータがデータベースに保存されていれば
-        if ($this->getLastConversationData($userId) !== PDO::PARAM_NULL) {
-            $lastConversationData = $this->getLastConversationData($userId);
+    //     // 前回までの会話のデータがデータベースに保存されていれば
+    //     if ($this->getLastConversationData($userId) !== PDO::PARAM_NULL) {
+    //         $lastConversationData = $this->getLastConversationData($userId);
 
-            // 前回までの会話のデータをパラメータに追加
-            $data["context"] = array(
-                "conversation_id" => $lastConversationData["conversation_id"],
-                "system" => array(
-                    "dialog_stack" => array(array("dialog_node" => $lastConversationData["dialog_node"])),
-                    "dialog_turn_counter" => 1,
-                    "dialog_request_counter" => 1
-                )
-            );
-        }
+    //         // 前回までの会話のデータをパラメータに追加
+    //         $data["context"] = array(
+    //             "conversation_id" => $lastConversationData["conversation_id"],
+    //             "system" => array(
+    //                 "dialog_stack" => array(array("dialog_node" => $lastConversationData["dialog_node"])),
+    //                 "dialog_turn_counter" => 1,
+    //                 "dialog_request_counter" => 1
+    //             )
+    //         );
+    //     }
 
-        // ConversationサービスのREST API
-        // chcp 65001 # コマンドプロンプトをutf-8にする
-        // curl -X POST -u "apikey:{apikey}" --header "Content-Type:application/json" --data "{\"input\": {\"text\": \"Hello\"}}" "{url}/v1/workspaces/{workspace_id}/message?version=2020-04-01"
-        $url = 'https://api.us-south.assistant.watson.cloud.ibm.com/v1/workspaces/' . getenv('WATSON_SKILL_ID') . '/message?version=2020-10-16';
-        // 新規セッションを初期化
-        $curl = curl_init($url);
+    //     // ConversationサービスのREST API
+    //     // chcp 65001 # コマンドプロンプトをutf-8にする
+    //     // curl -X POST -u "apikey:{apikey}" --header "Content-Type:application/json" --data "{\"input\": {\"text\": \"Hello\"}}" "{url}/v1/workspaces/{workspace_id}/message?version=2020-04-01"
+    //     $url = 'https://api.us-south.assistant.watson.cloud.ibm.com/v1/workspaces/' . getenv('WATSON_SKILL_ID') . '/message?version=2020-10-16';
+    //     // 新規セッションを初期化
+    //     $curl = curl_init($url);
 
-        // オプション
-        $options = array(
-            // コンテンツタイプ
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-            ),
-            // 認証用
-            CURLOPT_USERPWD => 'apikey:' . getenv('WATSON_API_KEY'),
-            // POST
-            CURLOPT_POST => true,
-            // 内容
-            CURLOPT_POSTFIELDS => json_encode($data),
-            // curl_exec時にboolenでなく取得結果を返す
-            CURLOPT_RETURNTRANSFER => true,
-        );
+    //     // オプション
+    //     $options = array(
+    //         // コンテンツタイプ
+    //         CURLOPT_HTTPHEADER => array(
+    //             'Content-Type: application/json',
+    //         ),
+    //         // 認証用
+    //         CURLOPT_USERPWD => 'apikey:' . getenv('WATSON_API_KEY'),
+    //         // POST
+    //         CURLOPT_POST => true,
+    //         // 内容
+    //         CURLOPT_POSTFIELDS => json_encode($data),
+    //         // curl_exec時にboolenでなく取得結果を返す
+    //         CURLOPT_RETURNTRANSFER => true,
+    //     );
 
-        // オプションを適用
-        curl_setopt_array($curl, $options);
-        // セッションを実行し結果を取得
-        $jsonString = curl_exec($curl);
-        // 文字列を連想配列に変換
-        $json = json_decode($jsonString, true);
-        // error_log(print_r($json, true));
-        // 会話データを取得
-        $conversationId = $json["context"]["conversation_id"];
-        $dialogNode = $json["context"]["system"]["dialog_stack"][0]["dialog_node"];
+    //     // オプションを適用
+    //     curl_setopt_array($curl, $options);
+    //     // セッションを実行し結果を取得
+    //     $jsonString = curl_exec($curl);
+    //     // 文字列を連想配列に変換
+    //     $json = json_decode($jsonString, true);
+    //     // error_log(print_r($json, true));
+    //     // 会話データを取得
+    //     $conversationId = $json["context"]["conversation_id"];
+    //     $dialogNode = $json["context"]["system"]["dialog_stack"][0]["dialog_node"];
 
-        // データベースに保存
-        $conversationData = array('conversation_id' => $conversationId, 'dialog_node' => $dialogNode);
-        $this->setLastConversationData($userId, $conversationData);
+    //     // データベースに保存
+    //     $conversationData = array('conversation_id' => $conversationId, 'dialog_node' => $dialogNode);
+    //     $this->setLastConversationData($userId, $conversationData);
 
-        // Conversationからの返答を取得
-        $outputText = $json['output']['text'][count($json['output']['text']) - 1];
-        ###############################
-        // $messages = [
-        //   "type" => "text",
-        //   "text" =>  $outputText
-        // ]
-        $messages = [
-            "to" => [$userId],
-            "type" => "text",
-            "text" =>  $outputText
-        ];
-        return $messages;
-    }
+    //     // Conversationからの返答を取得
+    //     $outputText = $json['output']['text'][count($json['output']['text']) - 1];
+    //     ###############################
+    //     // $messages = [
+    //     //   "type" => "text",
+    //     //   "text" =>  $outputText
+    //     // ]
+    //     // $messages = [
+    //     //     "to" => [$userId],
+    //     //     "type" => "text",
+    //     //     "text" =>  $outputText
+    //     // ];
+    //     return $outputText;
+    // }
 
-    # WATSON
-    // 会話データをデータベースに保存
-    function setLastConversationData($userId, $lastConversationData)
-    {
-        $conversationId = $lastConversationData['conversation_id'];
-        $dialogNode = $lastConversationData['dialog_node'];
+    // # WATSON
+    // // 会話データをデータベースに保存
+    // function setLastConversationData($userId, $lastConversationData)
+    // {
+    //     $conversationId = $lastConversationData['conversation_id'];
+    //     $dialogNode = $lastConversationData['dialog_node'];
 
-        if ($this->getLastConversationData($userId) === PDO::PARAM_NULL) {
-            $dbh = new dbConnection();
-            $dbh = dbConnection::getConnection();
-            $sql = 'insert into ' . TABLE_NAME_CONVERSATIONS . ' (conversation_id, dialog_node, userid) values (?, ?, pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'))';
-            $sth = $dbh->prepare($sql);
-            $sth->execute(array($conversationId, $dialogNode, $userId));
-        } else {
-            $dbh = new dbConnection();
-            $dbh = dbConnection::getConnection();
-            $sql = 'update ' . TABLE_NAME_CONVERSATIONS . ' set conversation_id = ?, dialog_node = ? where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
-            $sth = $dbh->prepare($sql);
-            $sth->execute(array($conversationId, $dialogNode, $userId));
-        }
-    }
+    //     if ($this->getLastConversationData($userId) === PDO::PARAM_NULL) {
+    //         $dbh = new dbConnection();
+    //         $dbh = dbConnection::getConnection();
+    //         $sql = 'insert into ' . TABLE_NAME_CONVERSATIONS . ' (conversation_id, dialog_node, userid) values (?, ?, pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'))';
+    //         $sth = $dbh->prepare($sql);
+    //         $sth->execute(array($conversationId, $dialogNode, $userId));
+    //     } else {
+    //         $dbh = new dbConnection();
+    //         $dbh = dbConnection::getConnection();
+    //         $sql = 'update ' . TABLE_NAME_CONVERSATIONS . ' set conversation_id = ?, dialog_node = ? where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
+    //         $sth = $dbh->prepare($sql);
+    //         $sth->execute(array($conversationId, $dialogNode, $userId));
+    //     }
+    // }
 
-    // データベースから会話データを取得
-    function getLastConversationData($userId)
-    {
-        $dbh = new dbConnection();
-        $dbh = dbConnection::getConnection();
-        $sql = 'select conversation_id, dialog_node from ' . TABLE_NAME_CONVERSATIONS . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
-        $sth = $dbh->prepare($sql);
-        $sth->execute(array($userId));
-        if (!($row = $sth->fetch())) {
-            return PDO::PARAM_NULL;
-        } else {
-            return array('conversation_id' => $row['conversation_id'], 'dialog_node' => $row['dialog_node']);
-        }
-    }
+    // // データベースから会話データを取得
+    // function getLastConversationData($userId)
+    // {
+    //     $dbh = new dbConnection();
+    //     $dbh = dbConnection::getConnection();
+    //     $sql = 'select conversation_id, dialog_node from ' . TABLE_NAME_CONVERSATIONS . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
+    //     $sth = $dbh->prepare($sql);
+    //     $sth->execute(array($userId));
+    //     if (!($row = $sth->fetch())) {
+    //         return PDO::PARAM_NULL;
+    //     } else {
+    //         return array('conversation_id' => $row['conversation_id'], 'dialog_node' => $row['dialog_node']);
+    //     }
+    // }
 }
