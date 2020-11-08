aa3f39783b2d0686b4c4a4830bdc385f9a7d1913
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-08 14:07:29 +0900

    done

--- app/Services/db.php
+++ app/Services/db.php
@@ -15,7 +15,7 @@ class dbConnection
   {
     // # ペットの呼び出し
     $name = DB::table('pets')->where('name', $word)->value('id');
-    $n = DB::table('pets')->where('name', "こ")->get();
+    $n = DB::table('pets')->where('name', "こ")->get()->first();
     error_log(gettype($n));
     error_log(json_encode($n, JSON_UNESCAPED_UNICODE));
     error_log($n);
