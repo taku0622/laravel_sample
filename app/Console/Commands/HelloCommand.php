2ec1e45f5fdea7329360d27ef51511abd90a1e98
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-12 01:08:12 +0900

    don

--- app/Console/Commands/HelloCommand.php
+++ app/Console/Commands/HelloCommand.php
@@ -38,13 +38,47 @@ class HelloCommand extends Command
     public function handle()
     {
         echo "hello command~~~" . PHP_EOL;
+        $contents = [
+            [
+                'title' => '2020年10月22日 3時限　並列・分散処理',
+                'content' => 'コンピュータサイエンス学部',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/',
+                'label' => '詳細'
+            ],
+            [
+                'title' => '2020年10月27日 1,2,3時限　スポーツ実技Ⅳ',
+                'content' => 'コンディショニングエクササイズ　工学部(学科共通), メディア学部, コンピュータサイエンス学部, 応用生物学部',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/',
+                'label' => '詳細'
+            ],
+            [
+                'title' => '2020年11月06日 1時限　知的財産権',
+                'content' => '工学部(学科共通)',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/',
+                'label' => '詳細'
+            ]
+        ];
         $message = [
             "to" => ["U6e0f4008a090ff5b5bef0323cae3428e"],
-            "type" => "text",
-            "text" => "質問"
+            "type" => "multiple",
+            "altText" =>  "休講案内",
+            "contents" => $contents
         ];
-        $message = array($message);
-        $data = json_encode($message, JSON_UNESCAPED_UNICODE);
+
+
+        // heroku logに表示
+        error_log("########################## push important info is ##########################");
+        error_log(json_encode($message, JSON_UNESCAPED_UNICODE));
+
+        // JSON形式への変換
+        // echo json_encode($object, JSON_UNESCAPED_UNICODE);
+
+        // JSON形式への変換
+        $data = json_encode([$message], JSON_UNESCAPED_UNICODE);
+        // echo "##################\n";
+        // echo gettype($data);
+        // echo $data;
+        // echo "##################\n";
         $options = array(
             'http' => array(
                 'method' => 'POST',
@@ -54,12 +88,11 @@ class HelloCommand extends Command
                 'content' => $data
             )
         );
-        error_log(json_encode($data, JSON_UNESCAPED_UNICODE));
-
         $context = stream_context_create($options);
-        $response = file_get_contents('https://tut-php-api.herokuapp.com/api', false, $context);
-        echo gettype($response);
+        $response = file_get_contents('https://tut-line-bot-test.glitch.me/push', false, $context);
+        // echo gettype($response);
         echo $response;
+
         // return 0;
     }
 }
