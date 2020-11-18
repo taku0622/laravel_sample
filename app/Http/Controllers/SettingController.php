5f340521e8e7b8f002565f83c1bdc48d17077d7f
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-18 14:51:57 +0900

    done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -10,6 +10,19 @@ class SettingController extends Controller
     {
         error_log("hello.....");
         error_log("request is" . $request->getContent());
+        $events = json_decode($request->getContent(), true);
+        $userId = $events["id"];
+        error_log($userId);
+        $number = $events["number"];
+        error_log($number);
+        $push_new = $events["push_new"];
+        error_log($push_new);
+        $push_important = $events["push_important"];
+        error_log($push_important);
+        $push_cancel = $events["push_cancel"];
+        error_log($push_cancel);
+        $push_event = $events["push_event"];
+        error_log($$push_event);
         return "success connect~";
     }
 }
