8d3923eb6c0940bdf4637f1e59ff6f4fb94339e3
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-18 14:19:50 +0900

    done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -9,6 +9,7 @@ class SettingController extends Controller
     public function insertStudent(Request $request)
     {
         error_log("hello.....");
+        error_log("request is" . $request->getContent());
         return "success connect~";
     }
 }
