efe8d74f8e7a0b9f03cbde4759da69baf3f7e6a9
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-20 19:00:46 +0900

    done

---
+++ database/migrations/2020_11_20_185712_create_informations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informations', function (Blueprint $table) {
+            $table->id(); //1
+            $table->text('title'); //2
+            $table->text('content'); //3
+            $table->text('uri'); //4
+            $table->date('posted_date'); //5
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informations');
+    }
+}
