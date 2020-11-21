379c7f06e9251fb6fdab577185162677a79f9dba
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-21 13:55:26 +0900

    done

--- database/migrations/2020_11_21_130824_create_tags5_table.php
+++ database/migrations/2020_11_21_130824_create_tags5_table.php
@@ -15,25 +15,25 @@ class CreateTags5Table extends Migration
     {
         Schema::create('tags5', function (Blueprint $table) {
             $table->id();
-            $table->boolean('all_department')->nullable();; // 1
-            $table->boolean('important')->nullable();; // 2
-            $table->boolean('cs')->nullable();; // 3
-            $table->boolean('bs')->nullable();; // 4
-            $table->boolean('es')->nullable();; // 5
-            $table->boolean('ms')->nullable();; // 6
-            $table->boolean('hs')->nullable();; // 7
-            $table->boolean('ds')->nullable();; // 8
-            $table->boolean('me')->nullable();; // 9
-            $table->boolean('ee')->nullable();; // 10
-            $table->boolean('ac')->nullable();; // 11
-            $table->boolean('nu')->nullable();; // 12
-            $table->boolean('ce')->nullable();; // 13
-            $table->boolean('pt')->nullable();; // 14
-            $table->boolean('ot')->nullable();; // 15
-            $table->boolean('mt')->nullable();; // 16
-            $table->boolean('inhachi')->nullable();; // 17
-            $table->boolean('inds')->nullable();; // 18
-            $table->boolean('inkogaku')->nullable();; // 19
+            $table->boolean('all_department')->nullable(); // 1
+            $table->boolean('important')->nullable(); // 2
+            $table->boolean('cs')->nullable(); // 3
+            $table->boolean('bs')->nullable(); // 4
+            $table->boolean('es')->nullable(); // 5
+            $table->boolean('ms')->nullable(); // 6
+            $table->boolean('hs')->nullable(); // 7
+            $table->boolean('ds')->nullable(); // 8
+            $table->boolean('me')->nullable(); // 9
+            $table->boolean('ee')->nullable(); // 10
+            $table->boolean('ac')->nullable(); // 11
+            $table->boolean('nu')->nullable(); // 12
+            $table->boolean('ce')->nullable(); // 13
+            $table->boolean('pt')->nullable(); // 14
+            $table->boolean('ot')->nullable(); // 15
+            $table->boolean('mt')->nullable(); // 16
+            $table->boolean('inhachi')->nullable(); // 17
+            $table->boolean('inds')->nullable(); // 18
+            $table->boolean('inkogaku')->nullable(); // 19
             $table->integer('information_id');
             $table->unique('information_id');
         });
