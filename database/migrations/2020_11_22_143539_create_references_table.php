a500f5cc57f6be68ee1745cf2de30ddf2c1b716a
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-22 14:36:15 +0900

    done

---
+++ database/migrations/2020_11_22_143539_create_references_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('references', function (Blueprint $table) {
+            $table->id();
+            $table->text('lecture_name'); //2
+            $table->text('teacher_name'); //3
+            $table->text('reference'); //4
+            $table->date('posted_date'); //5
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('references');
+    }
+}
